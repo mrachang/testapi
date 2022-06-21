c5ca30c2ffa195d9be69a6bc40a34a9bd8e0c477
mrachang <46752239+mrachang@users.noreply.github.com>
2022-06-21 14:01:54 +0800

    Add files via upload

--- src/main/java/com/example/linebot/handler/messageHandler.java
+++ src/main/java/com/example/linebot/handler/messageHandler.java
@@ -38,7 +38,7 @@ public class messageHandler {
 		JSONArray messages = new JSONArray();
 		JSONObject message = new JSONObject();
 		message.put("type", "text");
-		if("text"==text) {
+		if(text==text) {
 			message.put("text", "你好啊");
 				
 		}else { 
